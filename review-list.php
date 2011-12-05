539ffa5a30b933bf4239ceb4373c33bd67c2f3f6
Sean Watkins <slwatkins@uh.edu>
2011-12-05 15:56:21 -0600

    initial review list design

--- review-list.php
+++ review-list.php
@@ -1,55 +1,49 @@
-<table style="BORDER-RIGHT: black 1px solid; BORDER-TOP: black 1px solid; BORDER-LEFT: black 1px solid; BORDER-BOTTOM: black 1px solid">
+<ul id="review-az-list">
+            <li><a href="/index.php?page=numbers">#</a></li>
+            <li><a href="/index.php?page=a">A</a></li>
+            <li><a href="/index.php?page=b">B</a></li>
+            <li><a href="/index.php?page=c">C</a></li>
+            <li><a href="/index.php?page=d">D</a></li>
+            <li><a href="/index.php?page=e">E</a></li>
+            <li><a href="/index.php?page=f">F</a></li>
+            <li><a href="/index.php?page=g">G</a></li>
+            <li><a href="/index.php?page=h">H</a></li>
+            <li><a href="/index.php?page=i">I</a></li>
+            <li><a href="/index.php?page=j">J</a></li>
+            <li><a href="/index.php?page=k">K</a></li>
+            <li><a href="/index.php?page=l">L</a></li>
+            <li><a href="/index.php?page=m">M</a></li>
+            <li><a href="/index.php?page=n">N</a></li>
+            <li><a href="/index.php?page=o">O</a></li>
+            <li><a href="/index.php?page=p">P</a></li>
+            <li><a href="/index.php?page=q">Q</a></li>
+            <li><a href="/index.php?page=r">R</a></li>
+            <li><a href="/index.php?page=s">S</a></li>
+            <li><a href="/index.php?page=t">T</a></li>
+            <li><a href="/index.php?page=u">U</a></li>
+            <li><a href="/index.php?page=v">V</a></li>
+            <li><a href="/index.php?page=w">W</a></li>
+            <li><a href="/index.php?page=x">X</a></li>
+            <li><a href="/index.php?page=y">Y</a></li>
+            <li><a href="/index.php?page=z">Z</a></li>
+</ul>
+
+<h2 class="big-h2">Ten Most Recently Edited Game Reviews</h2>
+
+<table id="review-list">
+    <thead>
+    	<tr>
+            <th id="review-list-name">Name</th>
+            <th id="review-list-reviewer">Reviewer</th>
+            <th id="review-list-reviewed">Reviewed On</th>
+            <th id="review-list-publisher">Publisher</th>
+            <th id="review-list-designer">Designer</th>
+            <th id="review-list-published">Published In</th>
+            <th id="review-rating">Rating</th>
+        </tr>
+    </thead>
     <tbody>
         <tr>
-            <td style="WIDTH: 200px"><a href="/index.php?page=numbers">#</a></td>
-            <td style="WIDTH: 200px"><a href="/index.php?page=a">A</a></td>
-            <td style="WIDTH: 200px"><a href="/index.php?page=b">B</a></td>
-            <td style="WIDTH: 200px"><a href="/index.php?page=c">C</a></td>
-            <td style="WIDTH: 200px"><a href="/index.php?page=d">D</a></td>
-            <td style="WIDTH: 200px"><a href="/index.php?page=e">E</a></td>
-            <td style="WIDTH: 200px"><a href="/index.php?page=f">F</a></td>
-            <td style="WIDTH: 200px"><a href="/index.php?page=g">G</a></td>
-            <td style="WIDTH: 200px"><a href="/index.php?page=h">H</a></td>
-            <td style="WIDTH: 200px"><a href="/index.php?page=i">I</a></td>
-            <td style="WIDTH: 200px"><a href="/index.php?page=j">J</a></td>
-            <td style="WIDTH: 200px"><a href="/index.php?page=k">K</a></td>
-            <td style="WIDTH: 200px"><a href="/index.php?page=l">L</a></td>
-            <td style="WIDTH: 200px"><a href="/index.php?page=m">M</a></td>
-            <td style="WIDTH: 200px"><a href="/index.php?page=n">N</a></td>
-            <td style="WIDTH: 200px"><a href="/index.php?page=o">O</a></td>
-            <td style="WIDTH: 200px"><a href="/index.php?page=p">P</a></td>
-            <td style="WIDTH: 200px"><a href="/index.php?page=q">Q</a></td>
-            <td style="WIDTH: 200px"><a href="/index.php?page=r">R</a></td>
-            <td style="WIDTH: 200px"><a href="/index.php?page=s">S</a></td>
-            <td style="WIDTH: 200px"><a href="/index.php?page=t">T</a></td>
-            <td style="WIDTH: 200px"><a href="/index.php?page=u">U</a></td>
-            <td style="WIDTH: 200px"><a href="/index.php?page=v">V</a></td>
-            <td style="WIDTH: 200px"><a href="/index.php?page=w">W</a></td>
-            <td style="WIDTH: 200px"><a href="/index.php?page=x">X</a></td>
-            <td style="WIDTH: 200px"><a href="/index.php?page=y">Y</a></td>
-            <td style="WIDTH: 200px"><a href="/index.php?page=z">Z</a></td>
-        </tr>
-    </tbody>
-</table>
-
-<p></p><center><b><big>Ten Most Recently Edited Game Reviews</big></b></center><p></p>
-<p></p><p>
-
-
-</p>
-
-<table align="center" bgcolor="#fffff3" border="1" bordercolor="#000033" cellpadding="5" cellspacing="0">
-    <tbody>
-        <tr align="center" bgcolor="#ccccee" valign="top">
-            <td width="15%">Name</td>
-            <td width="15%">Reviewer</td>
-            <td width="10%">Reviewed On</td>
-            <td width="15%">Publisher</td>
-            <td width="15%">Designer</td>
-            <td width="10%">Published In</td>
-            <td width="10%">Rating</td>
-        </tr>
-        <tr align="center" valign="top">
             <td> <a href="http://www.dicetower.com/game_reviews/a/alien-frontiers-2.html">Alien Frontiers</a> </td>
             <td> Ryan Metzler </td>
             <td> 19 Nov 2011 </td>
@@ -58,7 +52,7 @@
             <td> 2010 </td>
             <td>  </td>
         </tr>
-        <tr align="center" valign="top">
+        <tr>
             <td> <a href="http://www.dicetower.com/game_reviews/p/potion-making-practice-2.html">Potion-Making: Practice</a> </td>
             <td> Ryan Metzler </td>
             <td> 19 Nov 2011 </td>
@@ -67,7 +61,7 @@
             <td> 2005 </td>
             <td>  </td>
         </tr>
-        <tr align="center" valign="top">
+        <tr>
             <td> <a href="http://www.dicetower.com/game_reviews/p/penny-arcade-the-card-game.html">Penny Arcade:  The Card Game</a> </td>
             <td> Ryan Metzler </td>
             <td> 18 Nov 2011 </td>
